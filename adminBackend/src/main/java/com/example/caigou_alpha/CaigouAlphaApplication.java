f0b39e3d3c260432ba05a9ec0cc47b872a80da11
Aye-zang <53073695+Aye-zang@users.noreply.github.com>
2021-04-27 15:42:55 +0800

    搭建springBoot框架

---
+++ adminBackend/src/main/java/com/example/caigou_alpha/CaigouAlphaApplication.java
@@ -0,0 +1,13 @@
+package com.example.caigou_alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaigouAlphaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaigouAlphaApplication.class, args);
+    }
+
+}
---
+++ adminBackend/src/test/java/com/example/caigou_alpha/CaigouAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caigou_alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaigouAlphaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
