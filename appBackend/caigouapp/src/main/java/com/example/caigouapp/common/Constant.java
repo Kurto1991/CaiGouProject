a7f59090365816b01324664d1c8e38014061f8d4
Kurto <18085604253@163.com>
2021-05-07 18:07:19 +0800

    添加返回结果类Result与常量类Constant

---
+++ appBackend/caigouapp/src/main/java/com/example/caigouapp/common/Constant.java
@@ -0,0 +1,10 @@
+package com.example.caigouapp.common;
+
+/**
+ * 常量类
+ */
+public class Constant {
+
+    public final static String SUCCESS = "200";
+    public final static String NOT_FOUND = "404";
+}
