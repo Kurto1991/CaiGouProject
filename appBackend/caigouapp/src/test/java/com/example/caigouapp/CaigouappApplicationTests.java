7f5c1fd61e9065ccc0cc72aaaf4861b57c8dc68e
Kurto <18085604253@163.com>
2021-05-07 15:43:56 +0800

    Create CaigouappApplicationTests.java

---
+++ appBackend/caigouapp/src/test/java/com/example/caigouapp/CaigouappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caigouapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaigouappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
