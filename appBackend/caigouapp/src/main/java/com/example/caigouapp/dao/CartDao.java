9ac080d27fb2d745370dcdee1ebc8dd3b9be3f5d
weihaochen111 <786576329@qq.com>
2021-05-10 22:35:52 +0800

    删除

--- appBackend/caigouapp/src/main/java/com/example/caigouapp/dao/CartDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.caigouapp.dao;
-
-import com.example.caigouapp.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartDao extends JpaRepository<Cart,Integer> {
-}
--- appBackend/caigouapp/src/main/java/com/example/caigouapp/dao/User_OrderDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.caigouapp.dao;
-
-import com.example.caigouapp.entity.User_Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface User_OrderDao extends JpaRepository<User_Order,Integer> {
-
-}
