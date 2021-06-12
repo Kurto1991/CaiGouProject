81bf8b22bbd12b00cf64b954aa7f1c4eede8f075
mingyu <1072876025@qq.com>
2021-06-12 19:22:27 +0800

    测试接口 fixed #23

--- appBackend/CaiGouApp/src/test/java/com/example/caigouapp/CaigouappApplicationTests.java
+++ appBackend/CaiGouApp/src/test/java/com/example/caigouapp/CaigouappApplicationTests.java
@@ -2,11 +2,9 @@ package com.example.caigouapp;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.example.caigouapp.dao.AddressDao;
 import com.example.caigouapp.dao.MenuDao;
-import com.example.caigouapp.entity.Food;
-import com.example.caigouapp.entity.Menu;
-import com.example.caigouapp.entity.MenuFood;
-import com.example.caigouapp.entity.MenuInfo;
+import com.example.caigouapp.entity.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +18,8 @@ class CaigouappApplicationTests {
 
     @Autowired
     MenuDao menuDao;
+    @Autowired
+    AddressDao addressDao;
 
     @Test
     void contextLoads() {
@@ -27,6 +27,11 @@ class CaigouappApplicationTests {
         strings.split("");
 
         System.out.println(strings);
+    }
+    @Test
+    void  c(){
+
+        System.out.println(addressDao.selectAddById(2));
     }
     @Test
      void getMenuinfo(){
---
+++ appBackend/CaiGouApp/src/test/java/com/example/caigouapp/service/MenuServiceTest.java
@@ -0,0 +1,8 @@
+package com.example.caigouapp.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuServiceTest {
+
+
+}
